528f98014139497b27e379813e5078e046755fd7
Max Serra Lasierra <mserralasierra@gmail.com>
2024-12-19 18:05:58 +0100

    move gnann_features.preprocess_data to gnann_data.preprocess_data

--- src/data/gnann_data.py
+++ src/data/gnann_data.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 from warnings import warn
 
 import pandas as pd
@@ -26,5 +26,25 @@ def load_and_merge_geo_csv_to_df(data_path: Path,
         except KeyError as e:
             warn(e)
             return None
+
+    return out_df
+
+
+def preprocess_data(df: pd.DataFrame,
+                    columns_renamer: Dict[str, str] = None) -> pd.DataFrame:
+
+    out_df = df.copy()
+
+    if "netrad" in out_df.columns:
+        out_df["netrad"] = out_df["netrad"] * 12.87
+        
+    if "evap" in out_df.columns:
+        out_df = out_df[out_df["evap"] < 10000]
+    
+    if "qr" in out_df.columns:
+        out_df.loc[out_df["qr"] < 0, "qr"] = 0
+
+    if columns_renamer is not None:
+        out_df.rename(columns=columns_renamer, inplace=True)
 
     return out_df
